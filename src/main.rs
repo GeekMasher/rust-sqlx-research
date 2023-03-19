4b4c1a3335ac3da297a599facf0c1e1287fb40ef
geekmasher <geekmasher@gmail.com>
2023-03-19 11:50:07 +0000

    Update 'src/main.rs'

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::env;
 use anyhow::Result;
 use sqlx::PgPool;
 
@@ -12,24 +11,26 @@ struct User {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // Create DB connection
+    // Create connection to Postgres DB
     let connection_url: String = std::env::var("DATABASE_URL")?;
     let pool = PgPool::connect(&connection_url).await?;
 
     // Input (default: "")
-    let input: String = env::args().last().unwrap_or(String::from(""));
+    let input: String = std::env::args().last().unwrap_or(String::from(""));
 
     // SQLx query_as macro (secure)
     let users: Vec<User> = sqlx::query_as!(
             User,
-            "SELECT * FROM users WHERE username LIKE $1 OR handle LIKE $2",
-            input_wide, input_wide
+            "SELECT * FROM users WHERE username = $1",
+            input
         )
         .fetch_all(&pool)
         .await?;
 
     // SQLx query_as function (secure)
-    let users: Vec<User> = sqlx::query_as::<_, User>("SELECT * FROM users WHERE username = $1")
+    let users: Vec<User> = sqlx::query_as::<_, User>(
+            "SELECT * FROM users WHERE username = $1"
+        )
         .bind(&input)
         .fetch_all(&pool)
         .await?;
@@ -41,14 +42,23 @@ async fn main() -> Result<()> {
         .fetch_all(&pool)
         .await?;
     
-    // [2]
+    // [2] SQLx query_as function using format string (insecure)
     let query = format!("SELECT * FROM users WHERE username = '{}'", &input);
     let users: Vec<User> = sqlx::query_as::<_, User>(&query)
         .fetch_all(&pool)
         .await?;
 
-    // Wide card input
+    // Wide card input (secure)
     let input_wide: String = format!("%{input}%");
+    let users: Vec<User> = sqlx::query_as!(
+            User,
+            "SELECT * FROM users WHERE username LIKE $1 OR handle LIKE $2",
+            input_wide, input_wide
+        )
+        .fetch_all(&pool)
+        .await?;
+
+
 
     // Print all the users
     for user in users {
